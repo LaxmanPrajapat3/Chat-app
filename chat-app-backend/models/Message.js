44a253957b100e1f9d8c84b4b05dd38f4d0d33c7
Laxman <theprogrammer452023@gmail.com>
2025-09-22 21:52:40 +0530

    solve an bug

---
+++ chat-app-backend/models/Message.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  from: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  to: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  message: { type: String },
+  file: { type: String }, // uploaded file URL
+  createdAt: { type: Date, default: Date.now }
+});
+
+// module.exports = mongoose.model('Message', messageSchema);
+const Message = mongoose.models.Message || mongoose.model("Message", messageSchema);
+
+module.exports=Message;
